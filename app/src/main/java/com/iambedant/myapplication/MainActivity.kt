0c72ed469892bca665f57146a8e36b5a02b52e56
Kuliza-233 <kuliza-233@Kuliza-233s-MacBook-Pro.local>
2018-05-13 14:34:09 +0530

    wip

--- app/src/main/java/com/iambedant/myapplication/MainActivity.kt
+++ app/src/main/java/com/iambedant/myapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Predicate
 import kotlinx.android.synthetic.main.activity_main.*
 import io.reactivex.subjects.PublishSubject
 
@@ -14,9 +15,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var disposable: Disposable
     private val dataSource: ArrayList<String> = ArrayList()
     lateinit var layoutManager: LinearLayoutManager
-    var subject = PublishSubject.create<Int>()
-
+    var subject = PublishSubject.create<ItemInfo>()
+    private val viewsViewed = java.util.ArrayList<Int>()
     private var startTime: Long = 0
+    private var endTime: Long = 0
 
     private var firstTrackFlag = false
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,18 +29,20 @@ class MainActivity : AppCompatActivity() {
         loadData()
         recyclerView.adapter = RvAdapter(dataSource)
         setRecyclerViewScrollListener()
-        disposable = subject.distinctUntilChanged().subscribe { i -> Log.d("RV", " $i ") }
+        disposable = subject.distinctUntilChanged().filter { i -> i.time > 300 }.subscribe { i -> Log.d("RV", " $i ") }
 
         recyclerView.viewTreeObserver
                 .addOnGlobalLayoutListener {
                     if (!firstTrackFlag) {
                         startTime = System.currentTimeMillis()
                         for (i in layoutManager.findFirstVisibleItemPosition()..layoutManager.findLastVisibleItemPosition()) {
-                            subject.onNext(i)
+                            viewsViewed.add(i)
                         }
                         firstTrackFlag = true
                     }
                 }
+
+
     }
 
     private fun loadData() {
@@ -51,9 +55,17 @@ class MainActivity : AppCompatActivity() {
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView?, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
+                    endTime = System.currentTimeMillis()
+                    for (i in viewsViewed) {
+                        subject.onNext(ItemInfo(i, startTime - endTime))
+                    }
+                    viewsViewed.clear()
+                }
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    startTime = System.currentTimeMillis()
                     for (i in layoutManager.findFirstVisibleItemPosition()..layoutManager.findLastVisibleItemPosition()) {
-                        subject.onNext(i)
+                        viewsViewed.add(i)
                     }
                 }
             }
